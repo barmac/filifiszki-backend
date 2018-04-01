0f8ae88cf7640998294f22998eab8da89d065307
Maciej Barelkowski <maciej@moodup.team>
2018-04-02 00:12:25 +0200

    Add bearer strategy with firebase

---
+++ functions/src/auth/strategies.ts
@@ -0,0 +1,31 @@
+import { admin } from '../config/firebase';
+import passportJs = require('passport');
+import { Strategy as BearerStrategy } from 'passport-http-bearer';
+
+const STRATEGY_BEARER = 'bearer';
+
+class AuthStrategies {
+  constructor(
+    private passport: passportJs.PassportStatic,
+    private firebaseAdmin: any
+  ) { }
+
+  public init() {
+    this.passport.use(new BearerStrategy((token, done) => this.authorizeToken(token, done)));
+  }
+
+  async authorizeToken(token: string, done: Function) {
+    try {
+      const decodedToken: admin.auth.DecodedIdToken = await admin.auth().verifyIdToken(token);
+
+      return done(null, decodedToken, { scope: 'all' });
+    } catch (error) {
+      return done(null, false);
+    }
+  }
+}
+
+const auth = new AuthStrategies(passportJs, admin);
+auth.init();
+
+export const isAuthenticated = passportJs.authenticate(STRATEGY_BEARER, { session: false });
